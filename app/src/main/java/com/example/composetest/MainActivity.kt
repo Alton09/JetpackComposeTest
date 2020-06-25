c406e10cb4a85e959b239ebe96feaa2bd58cbc5c
John Qualls <altonqualls@gmail.com>
2020-06-25 17:39:43 -0600

    Add progress bar

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.State
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModelProvider
 import androidx.ui.core.ContentScale
 import androidx.ui.core.Modifier
 import androidx.ui.core.clip
@@ -16,12 +16,12 @@ import androidx.ui.foundation.Text
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.layout.*
 import androidx.ui.livedata.observeAsState
+import androidx.ui.material.CircularProgressIndicator
 import androidx.ui.material.MaterialTheme
 import androidx.ui.res.imageResource
 import androidx.ui.text.style.TextOverflow
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
-import com.example.composetest.ui.ComposeTestTheme
 import com.example.composetest.ui.typography
 
 class MainActivity : AppCompatActivity() {
@@ -45,29 +45,43 @@ fun NewsStory(viewStateLiveData: LiveData<TestViewState>) {
     MaterialTheme {
         Column(
                 modifier = Modifier.padding(16.dp)
-        ){
+        ) {
             val imageModifier = Modifier
-                    .preferredHeightIn(maxHeight = 180.dp)
-                    .fillMaxWidth()
-                    .clip(shape = RoundedCornerShape(4.dp))
+                .preferredHeightIn(maxHeight = 180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp))
             Image(image, modifier = imageModifier, contentScale = ContentScale.Crop)
             Spacer(Modifier.preferredHeight(16.dp))
             Text(
-                    "A day wandering through the sandhills " +
-                            "in Shark Fin Cove, and a few of the " +
-                            "sights I saw",
-                    style = typography.h6,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis)
-            Text("Davenport, California",
-                style = typography.body2)
-            Text("December 2018",
-                style = typography.body2)
-            viewState.value?.retrievedItems?.forEach {
-                Text(it,
-                    style = typography.body2)
-            }
+                "A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                "Davenport, California",
+                style = typography.body2
+            )
+            Text(
+                "December 2018",
+                style = typography.body2
+            )
+
+            viewState.value?.let { renderViewState(it) }
         }
+    }
+}
+
+@Composable
+fun renderViewState(viewState: TestViewState) {
+    viewState.run {
+        retrievedItems.forEach {
+            Text(it, style = typography.body2)
+        }
+
+        if (loading) CircularProgressIndicator()
     }
 }
 
